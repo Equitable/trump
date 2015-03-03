05bf27c712f757068d014b6f1f7fcc033b8a654e
jnmclarty <jeffrey.mclarty@gmail.com>
2015-03-03 17:05:36 -0500

    DOC: Start README and setup

---
+++ README.rst
@@ -0,0 +1,42 @@
+=====
+Trump
+=====
+
+-----------
+Coming Soon
+-----------
+
+Dependencies
+=============
+Coming soon.
+
+Basic Usage
+===========
+Coming soon.
+
+Documentation
+=============
+Coming soon. For now, see the pdf in the prototype branch.
+
+Installation
+=============
+Check back March 22nd or slightly after.
+
+Documentation
+=============
+Check back March 22nd or slightly after.
+
+Contributing
+============
+Coming soon.
+
+License
+=======
+
+BSD-3 clause
+
+Background
+==========
+
+The prototype for ``Trump`` was built at Equitable Life of Canada in 2014 by Jeffrey McLarty, CFA 
+and Derek Vinke, CFA. 
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup
+
+def read(*p):
+    """Build a file path from paths and return the contents."""
+    with open(os.path.join(*p), 'r') as fi:
+        return fi.read()
+
+v = '0.0.1'
+
+setup(
+  name = 'Trump',
+  version = v,
+  packages = ['trump','smuggle','pandas','SQLAlchemy'],
+  description = 'Objectified Indexed Data',
+  long_description=(read('README.rst')),
+  author = 'Jeffrey McLarty',
+  author_email = 'jeffrey.mclarty@gmail.com',
+  url = 'https://github.com/Equitable/trump/'
+  download_url = 'https://github.com/Equitable/trump/tarball/' + v,
+  keywords = ['data', 'timeseries', 'time series', 'indexed', 'objectified', 'trump', 'monotonic', 'RDD', 'relational database', 'pandas', 'SQLAlchemy']
+  classifiers = ['Development Status :: 1 - Planning',
+                 'Intended Audience :: System Administrators',
+                 'License :: OSI Approved :: BSD License',
+                 'Topic :: Database',
+                 'Topic :: Office/Business',
+                 'Topic :: Scientific/Engineering',
+                 'Operating System :: OS Independent',             
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4'])
