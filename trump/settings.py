49a3efce805eac2ab1d3f1572213f5dd86be1a1e
jnmclarty <jeffrey.mclarty@gmail.com>
2015-03-04 15:41:13 -0500

    Reconsider configuration approach

--- trump/settings.py
+++ trump/settings.py
@@ -1,4 +1,33 @@
-dns = 'localhost:MYDB'
-host = 'localhost'
-user = 'admin'
-password = 'pass'
+def read_configuration():
+    """Reads the configuration for trump and the sources"""
+
+    import os
+    import ConfigParser
+    
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    config_dir = os.path.join(cur_dir,"config")
+    config_files = [(f,f[:-4]) for f in os.listdir(config_dir) if f[-4:] == ".cfg"]
+    
+    config = {}
+    
+    for fil,src in config_files:
+        config[src] = {}
+        cp = ConfigParser.ConfigParser()
+        cp.read(os.path.join(config_dir,fil))
+        for section in cp.sections():
+            config[src][section] = dict(cp.items(section))
+            section_args = list(config[src][section].keys())
+            for arg in section_args:
+                if arg != 'password':
+                    lu = {'TRUE' : True, 'FALSE' : False}
+                    if config[src][section][arg].upper() in lu.keys():
+                        config[src][section][arg] = lu[config[src][section][arg].upper()]
+                    elif config[src][section][arg].isdigit():
+                        config[src][section][arg] = int(config[src][section][arg])
+    
+    trumpcfg = config['trump']
+    del config['trump']
+    return trumpcfg, config
+
+if __name__ == '__main__':
+    trumpcfg, config = read_configuration()
