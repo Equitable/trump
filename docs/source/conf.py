cd3a728fcef7520e6c9f7b2f657c5ce0edf83df4
jnmclarty <jeffrey.mclarty@gmail.com>
2015-03-21 21:50:56 -0400

    Add mock for pandas for RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -268,3 +268,16 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+try:
+   from unittest.mock import MagicMock
+except ImportError:
+   from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
