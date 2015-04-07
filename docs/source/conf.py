9a05be05de492d05f2be621de74b9f2ad0156910
jnmclarty <jeffrey.mclarty@gmail.com>
2015-04-06 23:01:27 -0400

    Undo pygments style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,7 +94,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'fruity'
+pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
