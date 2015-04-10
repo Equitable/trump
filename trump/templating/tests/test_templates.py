7cbd21a050a9e94d0f8f1f5c3ce4f81c812e279c
Jeffrey McLarty <jnmclarty@users.noreply.github.com>
2015-04-09 23:17:56 -0400

    Add two tests for templates

--- trump/templating/tests/test_templates.py
+++ trump/templating/tests/test_templates.py
@@ -1,5 +1,5 @@
 
-from ..templates import QuandlFT
+from ..templates import QuandlFT, QuandlSecureFT, GoogleFinanceFT
 
 class TestTemplates(object):
 
@@ -8,3 +8,19 @@ class TestTemplates(object):
         assert ftemp.sourcing == {'authtoken': 'yyy',
                                   'trim_start': 'yyyy-mm-dd',
                                   'dataset': 'xxx'}
+
+    def test_quandl_secure_ft(self):
+        ftemp = QuandlSecureFT("xxx", trim_start="yyyy-mm-dd")
+        assert ftemp.sourcing == {'trim_start': 'yyyy-mm-dd',
+                                  'dataset': 'xxx'}
+        assert ftemp.meta == {'sourcing_key' : 'userone',
+                              'stype' : 'Quandl'}
+
+    def test_google_finance_ft(self):
+        ftemp = GoogleFinanceFT("xxx")
+        assert ftemp.sourcing == {'name': 'xxx',
+                                  'start': '2000-01-01,
+                                  'end': 'now',
+                                  'data_source' : 'google',
+                                  'data_column' : 'Close'}
+        assert ftemp.meta == {'stype' : 'PyDataDataReaderST'}
