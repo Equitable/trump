def9f204b562be1a9f18902ca68f90baa291259d
jnmclarty <jeffrey.mclarty@gmail.com>
2015-04-11 01:07:02 -0400

    Make options more robust, fall back to sample/default

--- trump/options.py
+++ trump/options.py
@@ -14,31 +14,65 @@ Parses config files at runtime, but still enables importing of this module
 import os
 import ConfigParser
 
+def get_from_nested(keys, adict):
+    print "gfn called on {} and {}".format(str(keys), str(adict))
+    if len(keys) > 0:
+        if keys[0] in adict:
+            return get_from_nested(keys[1:],adict[keys[0]])
+        else:
+            raise KeyError("{} not found.".format(keys[0]))
+    else:
+        return adict
 
-def _read_options(paths, fname=None):
+def _read_options(paths,fname_def=None):
     """Builds a configuration reader function"""
-    def reader_func(section=None):
+    def reader_func(fname=fname_def, sect=None, sett=None, default=None):
         """Reads the configuration for trump"""
 
         cur_dir = os.path.dirname(os.path.realpath(__file__))
         config_dir = os.path.join(cur_dir, *paths)
-        config_files = [(f, f[:-4])
+
+        config_files = [(f[:-4], f)
                         for f in os.listdir(config_dir) if f[-4:] == ".cfg"]
+        sample_files = [(f[:-11], f)
+                        for f in os.listdir(config_dir) if f[-11:] == ".cfg_sample"]
+
+        if fname:
+            config_files = [f for f in config_files if f[0] == fname]
+            sample_files = [f for f in sample_files if f[0] == fname]
+
+
+        config_files = dict(config_files)
+        sample_files = dict(sample_files)
+
+        cfg_files = sample_files
+        for fn, f in config_files.iteritems():
+            cfg_files[fn] = f
 
         confg = {}
 
-        for fil, src in config_files:
+        for src, fil in cfg_files.iteritems():
             confg[src] = {}
             cfpr = ConfigParser.ConfigParser()
             cfpr.read(os.path.join(config_dir, fil))
             for sec in cfpr.sections():
                 confg[src][sec] = dict(cfpr.items(sec))
 
-        if fname in confg:
-            if section in confg[fname]:
-                return confg[fname][section]
-            return confg[fname]
-        return confg
+        keys = []
+
+        if fname:
+            keys.append(fname)
+            if sect:
+                keys.append(sect)
+                if sett:
+                    keys.append(sett)
+        try:
+            return get_from_nested(keys, confg)
+        except KeyError:
+            if default is not None:
+                return default
+            else:
+                raise
 
     return reader_func
 
@@ -47,4 +81,13 @@ read_settings = _read_options(["templating", "settings"])
 
 if __name__ == '__main__':
     config = read_config()
-    settings = read_settings()
+    print config
+
+    eng_str = read_config(sect='readwrite', sett='engine')
+    print eng_str
+
+    settings = read_settings(fname='Quandl', sect='userone', sett='authtoken')
+    print settings
+
+    settings = read_settings(fname='Quandl', sect='userone', sett='authkey', default='XXXX')
+    print settings
