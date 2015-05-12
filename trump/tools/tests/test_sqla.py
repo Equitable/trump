46b7667bf704e98c52dbd9eb41a7ccbf6de9229e
jnmclarty <jeffrey.mclarty@gmail.com>
2015-05-11 23:34:26 -0400

    TST: Add test for repr mixin

---
+++ trump/tools/tests/test_sqla.py
@@ -0,0 +1,12 @@
+from ...orm import SetupTrump, SymbolManager
+
+class TestToolsSQLA(object):
+    
+    def setup_method(self, test_method):
+        self.eng = SetupTrump()
+        self.sm = SymbolManager(self.eng)
+
+    def test_repr_mixin(self):
+        sym = self.sm.create("testsym", overwrite=True)
+        assert repr(sym) == """Symbol(name=u'testsym', description=None, units=None, agg_method=u'priority_fill')"""
+        
