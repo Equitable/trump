c42b0dba7bd8353e42e5147d43c205f3cca95026
jnmclarty <jeffrey.mclarty@gmail.com>
2015-07-17 08:01:51 -0400

    Manual tweak to quandl

--- trump/extensions/source/quandl/quandlext.py
+++ trump/extensions/source/quandl/quandlext.py
@@ -5,22 +5,10 @@ renew = True
 class Source(object):
     def __init__(self, ses, **kwargs):
         import Quandl as q
-        data = q.get(**kwargs)
-        try:
-            fn = kwargs['fieldname']
-        except KeyError:
-            raise KeyError("fieldname wasn't specified in Quandl Feed")
-    
-        try:
-            data = data[fn]
-        except KeyError:
-            kemsg = """{} was not found in list of Quandle headers:\n
-                     {}""".format(fn, str(data.columns))
-            raise KeyError(kemsg)
+        self.q = q
 
     def getseries(self, ses, **kwargs):
-        import Quandl as q
-        data = q.get(**kwargs)
+        data = self.q.get(**kwargs)
         try:
             fn = kwargs['fieldname']
         except KeyError:
@@ -33,4 +21,4 @@ class Source(object):
                      {}""".format(fn, str(data.columns))
             raise KeyError(kemsg)
 
-    return data
+        return data
