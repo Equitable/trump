a807264df5cca56a2f3a1f8cd9755dfa80a27bda
jnmclarty <jeffrey.mclarty@gmail.com>
2015-07-20 17:06:47 -0400

    Handle NoData case for BBFetch

--- trump/extensions/source/tx-bbfetch/bbfetchext.py
+++ trump/extensions/source/tx-bbfetch/bbfetchext.py
@@ -10,29 +10,43 @@ Optional kwargs:
 - 'croptime' - boolean
 
 """
+
     
 stype = 'BBFetch'
 renew = True
 
 class Source(object):
     def __init__(self, ses, **kwargs):
-        import equitable.bpsdl as bf   
+        import pandas as pd
+        self.pd = pd
+        import equitable.bpsdl as bf  
+        from equitable.errors.existence import NoData
+        self.NoData = NoData
         self.bbapi = bf.Getter.BBapi(clean=False)
     def getseries(self, ses, **kwargs):
 		
         bbsec = self.bbapi.GetSecurity(kwargs['elid'])
 
         if kwargs['bbtype'] == 'COMMON':
-            data = bbsec.GetDataMostRecentFetchDaily(kwargs['valuefieldname'],KeepTime=False,KeepTimeZone=False)
+            # Bloomberg raises an exception, when there is no data returned.
+            # Trump, checks for the "no data", case using the "empty_feed" handle point.
+            # So, just pass it on, and let it go get caught/logged/whatever, at the handle point...
+            try:
+                data = bbsec.GetDataMostRecentFetchDaily(kwargs['valuefieldname'],KeepTime=False,KeepTimeZone=False)
+            except self.NoData:
+                data = self.pd.Series()
         elif kwargs['bbtype'] == 'BULK':            
             indexfilter = None
             
             if 'keep_list' in kwargs:
                 keep_list = kwargs['keep_list'].split(", ")
                 indexfilter = {'dividend_type' : keep_list}
+
+            try:                
+                data = bbsec.GetDataMostRecentFetchBulk(kwargs['fieldname'],indexfilter=indexfilter,columntoindex=kwargs['datecol'],datacolumn=kwargs['datacol'])
+            except self.NoData:
+                data = self.pd.Series()
                 
-            data = bbsec.GetDataMostRecentFetchBulk(kwargs['fieldname'],indexfilter=indexfilter,columntoindex=kwargs['datecol'],datacolumn=kwargs['datacol'])
-            
         try:
             dosum = kwargs['duphandler'] == 'sum'
         except:
